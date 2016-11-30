fbb8137d835dd657c87cb797b3a36acb0f2181f8
Sudheer Dandamudi <sudheer.dandamudi@gmail.com>
2016-11-30 17:35:19 -0500

    update

--- jenkins/main.tf
+++ jenkins/main.tf
@@ -4,14 +4,14 @@ provider "aws" {
 
 module "my_jenkins" {
   source = "git::https://github.com/skdandamudi/tf_module_aws_ec2.git"
-  ec2_machine_image = "${var.ec2_machine_image}"
-  number_of_instances = "${var.number_of_instances}"
-  ec2_instance_type = "${var.ec2_instance_type}"
-  ec2_user_login_key = "${var.ec2_user_login_key}"
-  subnet_id = "${var.subnet_id}"
-  security_groups =  "${var.security_groups)}"
-  root_volume_size = "${var.root_volume_size}"
-  app_name = "${var.app_name}"
-  app_env = "${var.app_env}"
+  ec2_machine_image = "ami-b73b63a0"
+  number_of_instances = "1"
+  ec2_instance_type = "m3.medium"
+  ec2_user_login_key = "navitas-devops-ssh"
+  subnet_id = "subnet-2370f30e"
+  security_groups =  "sg-d4691ba9 "
+  root_volume_size = "40"
+  app_name = "devops-jenkins-master"
+  app_env = "dev"
 
 }
