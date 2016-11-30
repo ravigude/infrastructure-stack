f65d3812d378789b393d8eb4dcadc42d8b5dd373
Sudheer Dandamudi <sudheer.dandamudi@gmail.com>
2016-11-30 17:36:41 -0500

    Fix Syntax error

--- jenkins/outputs.tf
+++ jenkins/outputs.tf
@@ -1,7 +1,7 @@
 output "ec2_instance_id" {
-  value = "${my_jenkins.ec2_instance_id}"
+  value = "${module.my_jenkins.ec2_instance_id}"
 }
 
 output "ec2_instance_ip" {
-  value = "${my_jenkins.ec2_instance_ip}"
+  value = "${module.my_jenkins.ec2_instance_ip}"
 }
