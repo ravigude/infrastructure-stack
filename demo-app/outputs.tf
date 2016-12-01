f15db964d569dd97ce01d58fa8e49c13c5cda422
Sudheer Dandamudi <sudheer.dandamudi@gmail.com>
2016-12-01 02:10:35 -0500

    update

--- demo-app/outputs.tf
+++ demo-app/outputs.tf
@@ -1,19 +1,16 @@
-output "asg_name" {
-	value = "${module.cwise_aws_waf_asg.asg_name}"
+
+output "demo_web_elb_dns_name" {
+	value = "${module.demo_web_elb.elb_dns_name}"
 }
 
-output "cwise_web_elb_dns_name" {
-	value = "${module.cwise_aws_web_elb.elb_dns_name}"
+output "demo_web_elb_name" {
+	value = "${module.demo_web_elb.elb_name}"
 }
 
-output "cwise_web_elb_name" {
-	value = "${module.cwise_aws_web_elb.elb_name}"
+output "demo_app_elb_dns_name" {
+	value = "${module.demo_app_elb.elb_dns_name}"
 }
 
-output "cwise_app_elb_dns_name" {
-	value = "${module.cwise_aws_app_elb.elb_dns_name}"
-}
-
-output "cwise_app_elb_name" {
-	value = "${module.cwise_aws_app_elb.elb_name}"
+output "demo_app_elb_name" {
+	value = "${module.demo_app_elb.elb_name}"
 }
